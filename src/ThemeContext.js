7bdaa207ec9a471b6ab95e1d222485e955d28577
Shams Blockchain dev <55982732+Shamsyum@users.noreply.github.com>
2021-01-18 09:20:33 +0500

    Update MainWithFunction.js and ThemeContext.js

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,4 +1,4 @@
-import createContext from "react";
+import {createContext} from "react";
 
 const ThemeContext = createContext({theme: "dark", setThemeMode: () => {}});
 export default ThemeContext;    
